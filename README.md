c99a37576c7004ce9cfd877562a83d661ad191db
henriqDLs <henrique.dalpisol@edu.pucrs.br>
2024-10-22 09:47:57 -0300

    Atualizando o readme e arrumando bug do prisma

--- README.md
+++ README.md
@@ -26,24 +26,49 @@
 
 [Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
 
-## Project setup
+### Passos para rodar o projeto
 
-```bash
-$ npm install
+Primeiramente, inicialize o AWS Lab e altere o arquivo `main.tf` substituindo as credenciais pelas fornecidas no seu lab.
+
+```hcl
+provider "aws" {
+  region = "us-east-1"
+
+  # Alterar access_key para sua aws_access_key_id
+  access_key = "SUA_AWS_ACCESS_KEY_ID"
+
+  # Alterar secret_key para sua aws_secret_access_key
+  secret_key = "SUA_AWS_SECRET_ACCESS_KEY"
+
+  # Alterar token para seu aws_session_token
+  token      = "SEU_AWS_SESSION_TOKEN"
+}
 ```
 
-## Compile and run the project
-
+Após isso, basta rodar o `terraform apply`
 ```bash
-# development
-$ npm run start
+# Inicializar o terraform
+terraform init
 
-# watch mode
-$ npm run start:dev
-
-# production mode
-$ npm run start:prod
+# Aplicar o terraform
+terraform apply
 ```
+
+Agora, já dentro do EC2 criado pelo terraform, rode os seguintes comandos:
+```bash
+# Inicializar o docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Clonar o repositório dentro da EC2
+git clone https://github.com/JoaoCampanher/csw24-grupod-TicketsOnFire-nestjs.git /home/ubuntu/nest-api
+cd /home/ubuntu/nest-api
+
+# Inicializar a API com o compose
+docker compose up -d
+```
+
+Por fim, basta pegar o ip público da sua EC2 e acessar a API na url `ipPublico:3000/api`
 
 ## Run tests
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIA23OPNELMSCOSBUAZ"
-  secret_key = "APEhI1rTen8DrBW6UPT7gdGf+ulDaPNa7OZo+8h5"
-  token      = "IQoJb3JpZ2luX2VjEDIaCXVzLXdlc3QtMiJGMEQCIA2OIQqcYSubiSS4EGR0YkgwLCuHIn8BK6O+K9td2cHJAiBEkcC6f5uyMUhnwVy/cYyKT+hEEJabu1bhFp49t6ZlbirBAgia//////////8BEAAaDDc0NjE0ODY2ODEyMSIMyW03zjCOm+utXOpXKpUCZNbaX1xjz+lfnOsS/J+hULGRFCpvIHWnjia0UhYX8hNfU6+oZMrB5R7/drsvhH9pnKUSTwZq33sMsCErVUfazOmpnf2it/sDn5oTdfxmhggvi1tCLsCagPqR2tTRyzY6KsJSdx/6ES4D4+Jdp332j8Rp3N6lH39HbKui18l4C1AJ4os98eCJKGihzq1/K3Tn7zbCibLkzzf8tEpZi7vTb3szzw3/7RfGPP7MbREpO6cOPOmW84bKRRR+5cUBzXvUzMlndlDJKuIOgLw4RsnQ2XPZ0hSrwrYYC13zYno1/7YaJfhwKPBW9kY6vvixZYRYFyd68NKLfRZEdUEaNCAlVb9ni32Xt49RNAI9HLvgsr6sh6RbZDDJ+9u4BjqeAVK2LOvIse+r5HyIQ2LK5o3v+m3Hd9vgHdPUu7rgAMEIGz+WyMzxFZkXcEPqrOAg5vGbXJTY+THNRpMhLN+aG6G+J4DajaR2utpiRf5QNMq/qhnRXvSS2uiIu0a+Z7A7k/6RXE6navW+MLvUJWnnzUeFZPJKgZUPXuYwb2rlO4LQSWWBZfj/v252zHNWOk9Xjeze8m27EuGlOF1QImtP"
+  access_key = ""
+  secret_key = ""
+  token      = ""
 }
 
 resource "aws_instance" "nest_ec2" {
-  ami           = "ami-0866a3c8686eaeeba" # Pode ser ajustado dependendo da região
+  ami           = "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
   key_name      = aws_key_pair.nest_key.key_name
 
@@ -27,13 +27,6 @@ resource "aws_instance" "nest_ec2" {
     sudo usermod -aG docker ubuntu
     sudo systemctl enable docker
     sudo systemctl start docker
-
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-
-    git clone https://github.com/JoaoCampanher/csw24-grupod-TicketsOnFire-nestjs.git /home/ubuntu/nest-api
-    cd /home/ubuntu/nest-api
-    docker-compose up -d
   EOF
 }
 
@@ -73,6 +66,14 @@ resource "aws_security_group" "nest_security_group" {
     description = "pg port"
     from_port   = 5432
     to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prisma Studio"
+    from_port   = 5555
+    to_port     = 5555
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
