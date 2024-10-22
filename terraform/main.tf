1b2b6de97f7cc6c083cb8ec3de75d0f78edfca68
henriqDLs <henrique.dalpisol@edu.pucrs.br>
2024-10-21 22:50:17 -0300

    Arrumando terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIA23OPNELMS2EAYV6T"
-  secret_key = "mwSMg7WrGmoVyhYarTRPEZLIiTggZM+sV7eW0WvP"
-  token      = "IQoJb3JpZ2luX2VjEBMaCXVzLXdlc3QtMiJHMEUCIQCeII5ed9ALsJ5zyS6bDFHrKQrXO0igmsKNC0b6HsdNogIgZcfKmqSnpnWBCHegoT2MZu0KLHm6eiO2Ob9laulihA0quAIIfBAAGgw3NDYxNDg2NjgxMjEiDERhEtqCrC80IdaXvyqVAt7/IEW7QR1ecDlulVVdS6jOFY9nmxT+HYY1GvwWHLliy0az97mpcpGijV8HiIpReIXhxiuR1dwGcu8xfNqgzixHMbxwb3Vk8BRiDg5RqoPebRjT9ideCd1+vDK6r0UDuMNYXwQtzIpolvOIfTWNo4serKQclCgRY/VdnqQbyMKZsQWGWSj7RqZGbV+tb4rHiRhiGxgZVi0p7h4Q4KaqlLC8vi6UeBgrkW5L08ODFY0P/c/HyIB1UNunXqgykHlAze4LzA+h99Ry713edEwlhhrf/Aiu8aQYT9B/b5U8S/EL8P4CxNtrhTAENBp4iMsPAi5MTjVV+LBo25MP/vIe1Bn7A32iTbhS8XzBTLiKcOoJFx0eO+swuZ/VuAY6nQEKo05tk3NLG0FE8LsjOl3DDxzGEZGNNK6W+G51WKnnVm887Jr8tDJOK1setFtVzr9fQreQC7WwC0Zp787gjcAtRivrrpHZXuZsXtybDzm7A+NrS6TdtozzZhNtc0p2hhEQ4LAAIEsdrpZA3vrTb3RZyQCi9Kn/Z6L6Qn7uh2zjkFxng7FY6DtW0jVSTicLKBiTa5h4hZ+FBqXe0yBM"
+  access_key = "ASIA23OPNELMSCOSBUAZ"
+  secret_key = "APEhI1rTen8DrBW6UPT7gdGf+ulDaPNa7OZo+8h5"
+  token      = "IQoJb3JpZ2luX2VjEDIaCXVzLXdlc3QtMiJGMEQCIA2OIQqcYSubiSS4EGR0YkgwLCuHIn8BK6O+K9td2cHJAiBEkcC6f5uyMUhnwVy/cYyKT+hEEJabu1bhFp49t6ZlbirBAgia//////////8BEAAaDDc0NjE0ODY2ODEyMSIMyW03zjCOm+utXOpXKpUCZNbaX1xjz+lfnOsS/J+hULGRFCpvIHWnjia0UhYX8hNfU6+oZMrB5R7/drsvhH9pnKUSTwZq33sMsCErVUfazOmpnf2it/sDn5oTdfxmhggvi1tCLsCagPqR2tTRyzY6KsJSdx/6ES4D4+Jdp332j8Rp3N6lH39HbKui18l4C1AJ4os98eCJKGihzq1/K3Tn7zbCibLkzzf8tEpZi7vTb3szzw3/7RfGPP7MbREpO6cOPOmW84bKRRR+5cUBzXvUzMlndlDJKuIOgLw4RsnQ2XPZ0hSrwrYYC13zYno1/7YaJfhwKPBW9kY6vvixZYRYFyd68NKLfRZEdUEaNCAlVb9ni32Xt49RNAI9HLvgsr6sh6RbZDDJ+9u4BjqeAVK2LOvIse+r5HyIQ2LK5o3v+m3Hd9vgHdPUu7rgAMEIGz+WyMzxFZkXcEPqrOAg5vGbXJTY+THNRpMhLN+aG6G+J4DajaR2utpiRf5QNMq/qhnRXvSS2uiIu0a+Z7A7k/6RXE6navW+MLvUJWnnzUeFZPJKgZUPXuYwb2rlO4LQSWWBZfj/v252zHNWOk9Xjeze8m27EuGlOF1QImtP"
 }
 
 resource "aws_instance" "nest_ec2" {
@@ -28,10 +28,10 @@ resource "aws_instance" "nest_ec2" {
     sudo systemctl enable docker
     sudo systemctl start docker
 
-    sudo curl -L "https://henriqDLs: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@github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
-    git clone https://henriqDLs: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@github.com/JoaoCampanher/csw24-grupod-TicketsOnFire-nestjs.git /home/ubuntu/nest-api
+    git clone https://github.com/JoaoCampanher/csw24-grupod-TicketsOnFire-nestjs.git /home/ubuntu/nest-api
     cd /home/ubuntu/nest-api
     docker-compose up -d
   EOF
